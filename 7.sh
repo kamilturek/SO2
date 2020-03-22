b1cc0fb593538d0f5947b9e4dafd409f5cfd008d
Kamil Turek <tazzu@live.com>
2020-03-22 16:58:42 +0100

    7.sh

--- 7.sh
+++ 7.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     echo "Invalid number of arguments"
     exit 1
 fi
 
-if [ ! -d "$1" ]; then
+if [ ! -d $1 ]; then
     echo "$1: file does not exist or is not a directory"
     exit 1
 fi
 
 for file in $(ls -A $1); do
-    if [ -f $1/$file ] && [ -w $1/$file ]; then
-        echo $file
-        echo
-        cat $1/$file
-        echo
+    if [ $1/$file != $1/$2 ] && [ ! -s $1/$file ] && [ -f $1/$file ]; then
+        rm $1/$file
+        echo "$1/$file" >> $1/$2
     fi
 done
