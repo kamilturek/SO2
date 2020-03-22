dede129b3ea0ac64f43fb96473933073a298b266
Kamil Turek <tazzu@live.com>
2020-03-22 19:11:17 +0100

    10.sh

---
+++ 10.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Invalid number of arguments"
+    exit 1
+fi
+
+if [ ! -d $1 ]; then
+    echo "$1: file does not exist or is not a directory"
+    exit 1
+fi
+
+if [ ! -d $2 ]; then
+    echo "$2: file does not exist or is not a directory"
+    exit 1
+fi
+
+for file in $(ls -A $1); do
+    if [ -f $1/$file ] && [ -f $2/$file ]; then
+        rm $1/$file
+    fi
+done
