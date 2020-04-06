7bb961c9c387ab06b0d4788c7f5c50dc057c3aa5
Kamil Turek <tazzu@live.com>
2020-04-06 21:15:03 +0200

    Update 4.sh

--- links/4.sh
+++ links/4.sh
@@ -10,19 +10,12 @@ if [ ! -d $1 ]; then
     exit 1
 fi
 
-changed=1
-
-while [ $changed -eq 1 ]; do
-    changed=0
-    for i in $(ls -A $1); do
-        for j in $(ls -A $1); do
-            if [ $1/$i != $1/$j ]; then
-                if [ ! -h $1/$i ] && [ ! -h $1/$j ] && [ $1/$i -ef $1/$j ]; then
-                    ln -fs $(realpath $1/$i) $1/$j
-                    changed=1
-                    break 2
-                fi
+for i in $(ls -A $1); do
+    for j in $(ls -A $1); do
+        if [ $1/$i != $1/$j ] && [ $1/$i -ef $1/$j ]; then
+            if [ -f $1/$i ] && [ ! -h $1/$i ] && [ ! -h $1/$j ]; then
+                ln -fs $(realpath $1/$i) $1/$j
             fi
-        done
+        fi
     done
 done
